de54d5019a1f5d0706dff5b5c15a722aaf77879e
haybales <haleynicole126@gmail.com>
2022-09-21 21:42:25 -0600

    javascript quiz and question structure

---
+++ app.js
@@ -0,0 +1,7 @@
+var questions = [
+    new Questions("Which one is not an object oriented programming language?", ["Java", "C#", "C++", "C"], "C"),
+    new Questions("Which language is used for styling web pages?", ["HTML", "JQuery", "CSS", "XML"], "CSS"),
+    new Questions("There are ____ main components of object oriented programming.", ["1", "6", "2", "4"], "4"),
+    new Questions("Which language is used for web apps?", ["PHP", "Python", "Javascript", "All"], "All"),
+    new Questions("MVC Is a ____.", ["Language", "Library", "Framework", "All"], "Framework")
+];
